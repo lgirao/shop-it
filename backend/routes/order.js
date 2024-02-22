3119b7ab25342e3ae6646c21f1b8492d72bdbb0d
lgirao <lesleygirao@gmail.com>
2024-02-22 14:05:37 -0500

    sales endpoint

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 import { authorizeRoles, isAuthenticatedUser } from "../middleware/auth.js";
-import { deleteOrder, getAllOrders, getOrderDetails, getUserOrder, newOrder, updateOrder } from "../controllers/orderControllers.js";
+import { deleteOrder, getAllOrders, getOrderDetails, getSales, getUserOrder, newOrder, updateOrder } from "../controllers/orderControllers.js";
 
 
 router.route("/orders/new").post(isAuthenticatedUser, newOrder);
@@ -17,5 +17,10 @@ router.route("/admin/orders/:id")
     .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
     .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder)
 ;
+router.route("/admin/get_sales").get(
+    isAuthenticatedUser, 
+    authorizeRoles("admin"),
+    getSales
+);
 
 export default router;
--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,54 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MetaData from '../layout/MetaData';
 import AdminLayout from '../layout/AdminLayout';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import SalesChart from "../charts/SalesChart";
 
 const Dashboard = () => {
-  return <AdminLayout>Dashboard</AdminLayout>
-  // return (
-  //   <>
-  //     <MetaData title={"Dashboard"} />
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-  //     <div class="d-flex justify-content-start align-items-center">
-  //       <div class="mb-3 me-4">
-  //         <label class="form-label d-block">Start Date</label>
-  //         <input type="date" class="form-control" />
-  //       </div>
-  //       <div class="mb-3">
-  //         <label class="form-label d-block">End Date</label>
-  //         <input type="date" class="form-control" />
-  //       </div>
-  //       <button class="btn fetch-btn ms-4 mt-3 px-5">Fetch</button>
-  //     </div>
+  const submitHandler = () => {
+    console.log(new Date(startDate).toISOString());
+    console.log(endDate.toISOString());
+  }
 
-  //     <div class="row pr-4 my-5">
-  //       <div class="col-xl-6 col-sm-12 mb-3">
-  //         <div class="card text-white bg-success o-hidden h-100">
-  //           <div class="card-body">
-  //             <div class="text-center card-font-size">
-  //               Sales
-  //               <br />
-  //               <b>$0.00</b>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
+  return (
+    <>
+      <MetaData title={"Dashboard"} />
 
-  //       <div class="col-xl-6 col-sm-12 mb-3">
-  //         <div class="card text-white bg-danger o-hidden h-100">
-  //           <div class="card-body">
-  //             <div class="text-center card-font-size">
-  //               Orders
-  //               <br />
-  //               <b>0</b>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
+      <AdminLayout>
+        <div className="d-flex justify-content-start align-items-center">
+          <div className="mb-3 me-4">
+            <label className="form-label d-block">Start Date</label>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              className='form-control'
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label d-block">End Date</label>
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              className='form-control'
+            />
+          </div>
+          <button className="btn fetch-btn ms-4 mt-3 px-5" onClick={submitHandler}>Fetch</button>
+        </div>
 
-  //     <div class="mb-5"></div>
-  //   </>
-  // )
+        <div className="row pr-4 my-5">
+          <div className="col-xl-6 col-sm-12 mb-3">
+            <div className="card text-white bg-success o-hidden h-100">
+              <div className="card-body">
+                <div className="text-center card-font-size">
+                  Sales
+                  <br />
+                  <b>$0.00</b>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-6 col-sm-12 mb-3">
+            <div className="card text-white bg-danger o-hidden h-100">
+              <div className="card-body">
+                <div className="text-center card-font-size">
+                  Orders
+                  <br />
+                  <b>0</b>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <SalesChart />
+        
+        <div className="mb-5"></div>
+      </AdminLayout>
+    </>
+  )
 }
 
 export default Dashboard;
