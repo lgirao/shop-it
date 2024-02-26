648505e3b164824383455d8cc98b2a57822a685a
lgirao <lesleygirao@gmail.com>
2024-02-26 10:06:49 -0500

    product details edit

--- frontend/src/components/product/ProductDetails.jsx
+++ frontend/src/components/product/ProductDetails.jsx
@@ -142,7 +142,7 @@ const ProductDetails = () => {
           type="button"
           id="cart_btn"
           className="btn btn-primary d-inline ms-4"
-          disabled={product.stock <= 0}
+          disabled={product?.stock <= 0}
           onClick={setItemToCart}
         >
           Add to Cart
